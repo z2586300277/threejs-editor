6fd7d00ec8ae2f634c178e04de1e66aaf2a0d69e
zhangjingguang <2586300277@qq.com>
2025-03-10 11:31:00 +0800

    page

---
+++ src/editor/editor.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="edit" ref="editor"> </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { ThreeEditor } from '../../../Vite_three-editor/dist/index'
+// import { ThreeEditor } from 'three-edit-cores'
+
+const editor = ref(null)
+
+onMounted(() => {
+
+    new ThreeEditor(editor.value, {
+
+        fps: null,
+
+        pixelRatio: window.devicePixelRatio * 1,
+
+        webglRenderParams: { antialias: true, alpha: true, logarithmicDepthBuffer: true },
+
+        sceneParams: null
+
+    })
+
+});
+
+</script>
+
+<style scoped>
+.edit {
+    width: 100vw;
+    height: 100vh;
+}
+</style>
