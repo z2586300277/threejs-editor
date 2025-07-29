dd21897ba998e17178cb9e0aee3bab2285ed77a0
zhangjingguang <2586300277@qq.com>
2025-07-29 11:46:19 +0800

    新案例

--- docs/config.js
+++ docs/config.js
@@ -7,6 +7,8 @@ window.editorJsons = [
     'editorJson/城市效果.json',
     'editorJson/地图.json',
     'editorJson/卫星地图.json',
+    'editorJson/火烧云.json',
+    'editorJson/魔法战士.json',
     'editorJson/内嵌网页.json',
     'editorJson/草原追逐.json',
     'editorJson/车展.json',
