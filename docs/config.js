ad1f318c54d5efbcdc3c1972bd536342ec80fe02
zhangjingguang <2586300277@qq.com>
2025-08-01 18:12:10 +0800

    优雅园区

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,7 @@
 window.editorJsons = [
     'editorJson/优雅永不过时.json',
     'editorJson/B站视频配置.json',
+    'editorJson/优雅园区.json',
     'editorJson/瓦片3dtiles模型.json',
     'editorJson/自定义着色器.json',
     'editorJson/园区.json',
